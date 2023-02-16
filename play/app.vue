b6d342a17b0ab3d18d739f6c3da57bfc216eeea8
shiyue0101 <shiyue0101@outlook.com>
2023-02-17 01:00:06 +0800

    chore: 增加加载组件演示

--- play/app.vue
+++ play/app.vue
@@ -6,14 +6,35 @@
     <dtsz-button type="info">info</dtsz-button>
     <dtsz-button type="warning">warning</dtsz-button>
     <dtsz-button type="danger">danger</dtsz-button>
-    <dtsz-button
-      type="danger"
-      round
-      disabled
-    ></dtsz-button>
+    <dtsz-button type="danger" round disabled></dtsz-button>
     <DIcon name="edit" />
   </div>
+  <div v-loading:[title]="loading">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+      veritatis saepe, exercitationem illo at ea consectetur nam dolorum
+      consequuntur totam, nobis consequatur et voluptate enim vel fugit
+      aspernatur reiciendis asperiores!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+      veritatis saepe, exercitationem illo at ea consectetur nam dolorum
+      consequuntur totam, nobis consequatur et voluptate enim vel fugit
+      aspernatur reiciendis asperiores!
+    </p>
+  </div>
 </template>
+
 <script lang="ts" setup>
-import { DtszButton, DIcon } from 'dtsz-ui'
+import { DtszButton, DIcon } from "dtsz-ui";
+import { ref, onMounted } from "vue";
+
+const loading = ref<boolean>(true);
+const title = ref<string | undefined>("Loading...");
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 3000);
+});
 </script>
