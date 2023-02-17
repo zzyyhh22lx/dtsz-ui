624a4c4a56ee83ead6d6ddc1a9a888914183d02d
zzyyhh22lx <2744732798@qq.com>
2023-02-17 22:16:37 +0800

    fix: 测试

--- play/app.vue
+++ play/app.vue
@@ -5,9 +5,9 @@
     <dtsz-button type="success">success</dtsz-button>
     <dtsz-button type="primary">primary</dtsz-button>
     <dtsz-button type="info">info</dtsz-button>
-    <dtsz-button type="warning">warning</dtsz-button>
     <dtsz-button type="danger">danger</dtsz-button>
     <dtsz-button type="danger" round disabled></dtsz-button>
+    <dtsz-button type="warning">warning</dtsz-button>
 
     <DIcon name="edit" />
   
