d5f1ddc5fc5030f34f20fa9e5cb448bb91636068
shiyue0101 <shiyue0101@outlook.com>
2023-02-20 22:20:52 +0800

    chore: 导入 loading 组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,15 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
-import { DtszButton, DProgressbar, DIcon } from 'dtsz-ui'
+import { DtszButton, DProgressbar, DIcon } from "dtsz-ui";
+import { loadingDirective } from "dtsz-ui/src/loading/index";
+
 export default {
   ...DefaultTheme,
-  NotFound: () => 'custom 404',
+  NotFound: () => "custom 404",
   enhanceApp({ app }) {
-    app.component('DtszButton', DtszButton)
-    app.component('DProgressbar', DProgressbar)
-    app.component('DIcon', DIcon)
-  }
+    app.component("DtszButton", DtszButton);
+    app.component("DProgressbar", DProgressbar);
+    app.component("DIcon", DIcon);
+    app.directive("loading", loadingDirective);
+  },
 };
