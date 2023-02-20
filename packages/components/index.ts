7c32fe5b78bc298cd483993e3f3ceaa8fee0d0ed
zzyyhh22lx <2744732798@qq.com>
2023-02-20 17:00:15 +0800

    fix: 修改

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,5 +1,8 @@
-import DtszButton from './src/button'
-import DIcon from './src/icon'
-import DProgressbar from './src/progressbar'
-
-export { DtszButton, DIcon, DProgressbar }
+import components from './src/index'
+export * from './src/index'
+import { App } from 'vue'
+export default {
+    install: (app: App) => {
+        components.forEach(c => app.use(c));
+    }
+}
---
+++ packages/components/src/index.ts
@@ -0,0 +1,6 @@
+import DtszButton from './button'
+import DIcon from './icon'
+import DProgressbar from './progressbar'
+
+export { DtszButton, DIcon, DProgressbar }
+export default [DtszButton, DIcon, DProgressbar] 
