4a621f99762eac3391e2c9d28da4e0250e87f2b5
shiyue0101 <shiyue0101@outlook.com>
2023-02-20 13:55:33 +0800

    chore: 修改引入方式

--- play/app.vue
+++ play/app.vue
@@ -1,40 +1,7 @@
 <template>
-  <div>
-    <dtsz-button></dtsz-button>
-    <dtsz-button type="success">success</dtsz-button>
-    <dtsz-button type="primary">primary</dtsz-button>
-    <dtsz-button type="info">info</dtsz-button>
-    <dtsz-button type="warning">warning</dtsz-button>
-    <dtsz-button type="danger">danger</dtsz-button>
-    <dtsz-button type="danger" round disabled></dtsz-button>
-    <DIcon name="edit" />
-  </div>
-  <div v-loading:[title]="loading">
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
-      veritatis saepe, exercitationem illo at ea consectetur nam dolorum
-      consequuntur totam, nobis consequatur et voluptate enim vel fugit
-      aspernatur reiciendis asperiores!
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
-      veritatis saepe, exercitationem illo at ea consectetur nam dolorum
-      consequuntur totam, nobis consequatur et voluptate enim vel fugit
-      aspernatur reiciendis asperiores!
-    </p>
-  </div>
+  <sy></sy>
 </template>
 
 <script lang="ts" setup>
-import { DtszButton, DIcon } from "dtsz-ui";
-import { ref, onMounted } from "vue";
-
-const loading = ref<boolean>(true);
-const title = ref<string | undefined>("Loading...");
-
-onMounted(() => {
-  setTimeout(() => {
-    loading.value = false;
-  }, 3000);
-});
+import sy from "./components/sy.vue";
 </script>
---
+++ play/components/sy.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-loading:[text]="loading">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+      veritatis saepe, exercitationem illo at ea consectetur nam dolorum
+      consequuntur totam, nobis consequatur et voluptate enim vel fugit
+      aspernatur reiciendis asperiores!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+      veritatis saepe, exercitationem illo at ea consectetur nam dolorum
+      consequuntur totam, nobis consequatur et voluptate enim vel fugit
+      aspernatur reiciendis asperiores!
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
+const loading = ref<boolean>(true);
+const text = ref<string | undefined>("Loading...");
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 3000);
+});
+</script>
+
+<script lang="ts">
+export default { name: "Sy" };
+</script>
