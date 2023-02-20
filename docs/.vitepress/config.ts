ddf0f16964af6ea8daa526aa485014fb7697b95a
shiyue0101 <shiyue0101@outlook.com>
2023-02-20 21:55:06 +0800

    chore: 侧栏增加反馈组件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,29 +1,29 @@
 export default {
   themeConfig: {
-    siteTitle: 'Dtsz-ui🤣',
+    siteTitle: "Dtsz-ui🤣",
     footer: {
-      message: '快来加入Dtsz吧🤩<a>https://github.com/zzyyhh22lx/dtsz-ui</a>',
+      message: "快来加入Dtsz吧🤩<a>https://github.com/zzyyhh22lx/dtsz-ui</a>",
     },
     sidebar: [
       {
-        text: '基本配置',
-        items: [
-          {text: '导入', link: '/component/index'}
-        ]
+        text: "基本配置",
+        items: [{ text: "导入", link: "/component/index" }],
       },
       {
-        text: '基础组件',
+        text: "基础组件",
         items: [
-          { text: '按钮', link: '/component/button' },
-          { text: '图标', link: '/component/icon' }
-        ]
+          { text: "按钮", link: "/component/button" },
+          { text: "图标", link: "/component/icon" },
+        ],
       },
       {
-        text: '进阶组件',
-        items: [
-          { text: '环形进度条', link: '/component/progress' }
-        ]
-      }
-    ]
-  }
-}
+        text: "反馈组件",
+        items: [{ text: "加载", link: "/component/loading" }],
+      },
+      {
+        text: "进阶组件",
+        items: [{ text: "环形进度条", link: "/component/progress" }],
+      },
+    ],
+  },
+};
